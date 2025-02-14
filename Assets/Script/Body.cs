a71893e4ff8c2fe26d9aa62be021cd36eaf30339
JaeHyeong98 <dks66092487@gmail.com>
2025-02-14 13:53:16 +0900

    다리 움직임 수정 중

--- Assets/Script/Body.cs
+++ Assets/Script/Body.cs
@@ -1,20 +1,29 @@
 using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Body : MonoBehaviour
 {
     private Transform parent;
+    public List<Transform> legs;
+    public List<LegEndPoint> legsEnd;
 
     private void Awake()
     {
         parent = transform.parent.transform;
-    }
-    public void MoveBody(Vector3 vec)
-    {
-         // 다리가 이동한 거리의 70%
-        //transform.position = vec;
 
+        for(int i = 0;i < transform.childCount;i++)
+        {
+            if (transform.GetChild(i).gameObject.name.Contains("Leg"))
+                legs.Add(transform.GetChild(i));
+        }
+    }
+
+    public void MoveBody(Vector3 vec,string name)
+    {
         StartCoroutine(BodyCorrection(vec));
+        StartCoroutine(OtherLegCorrection(vec,name));
     }
 
     IEnumerator BodyCorrection(Vector3 vec)
@@ -23,14 +32,44 @@ public class Body : MonoBehaviour
         Debug.Log("[body] " + parent.transform.position);
         Debug.Log("[body] " + pNextPos);
 
-        for (int i = 0; i < 10; i++) 
+        while(Vector3.Distance(parent.transform.position, pNextPos) > 0.1f)
         {
             parent.transform.position = Vector3.MoveTowards(parent.transform.position, pNextPos, 0.1f);
             yield return new WaitForSeconds(0.2f);
 
-            Debug.Log("[body] "+parent.transform.position);
+            if (Vector3.Distance(parent.transform.position, pNextPos) <= 0.1f)
+                break;
         }
         
+        yield return null;
+    }
+
+    IEnumerator OtherLegCorrection(Vector3 vec,string name)
+    {
+        int n = int.Parse(name.Split("_")[1]);
+        Vector3 reversVec = legs[n].position - legsEnd[n].transform.position;
+        Debug.Log(reversVec);
+        Debug.Log(vec);
+
+        //while (true)
+        //{
+        //    Debug.Log("[body] leg point move");
+        //    for (int i = 0; i < legsEnd.Count; i++)
+        //    {
+        //        if (i == n)
+        //        {
+        //            legsEnd[i].transform.position = Vector3.MoveTowards(legsEnd[i].transform.position, legs[i].position, 0.1f);
+        //        }
+        //        else
+        //        {
+        //            legsEnd[i].transform.position = Vector3.MoveTowards(legsEnd[i].transform.position, legsEnd[i].transform.position+reversVec, 0.1f);
+        //        }
+        //
+        //        yield return new WaitForSeconds(0.2f);
+        //    }
+        //    if (Vector3.Distance(legsEnd[legsEnd.Count - 1].transform.position, legs[legsEnd.Count - 1].position) <= 3.65f)
+        //        break;
+        //}
         yield return null;
     }
 }
--- Assets/Script/LegEndPoint.cs
+++ Assets/Script/LegEndPoint.cs
@@ -20,11 +20,28 @@ public class LegEndPoint : MonoBehaviour
     {
         curState = PointState.Idle;
         col = GetComponent<Collider>();
-        if (col != null)
-            Debug.Log("find col");
 
         prePos = transform.position;
         curPos = transform.position;
+
+        body.legsEnd.Add(this);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground") && curState == PointState.TakeOff) // 공중에 떠있는 상태에서 타겟이 땅에 다인 경우
+        {
+            curState = PointState.Land; // 타겟이 땅에 내린 상태
+            LandAction();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground") && curState == PointState.Idle) // 현재 땅에서 가만히 있는 상태에서 탈출 할 때
+        {
+            curState = PointState.TakeOff; // 타겟이 공중에 뜬 상태
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -53,18 +70,6 @@ public class LegEndPoint : MonoBehaviour
         curState = PointState.Idle;
         Vector3 change = Vector3.Lerp(curPos - prePos, Vector3.zero, 0.3f); // 다리 이동거리의 70%
 
-        body.MoveBody(change);
-        StartCoroutine(BodyCorrection(change));
-    }
-
-    IEnumerator BodyCorrection(Vector3 vec)
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, Vector3.zero, 0.1f);
-            yield return new WaitForSeconds(0.2f);
-        }
-
-        yield return null;
+        body.MoveBody(change,transform.name);
     }
 }
